5b2bb3c0b896da50e4b00661260b3bc32d15a8e5
Ji Kang <jkang18@uw.edu>
2021-12-01 18:38:59 -0800

    bash script to load chromedriver executable for selenium

---
+++ setup_selenium.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl https://chromedriver.storage.googleapis.com/95.0.4638.69/chromedriver_linux64.zip --output chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+mv ./chromedriver ./web_scraper/chromedriver
+
+rm chromedriver_linux64.zip

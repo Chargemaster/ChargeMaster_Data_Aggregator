9a349a528726b6bb732b65aef63614039a6bad4c
Ji Kang <jkang18@uw.edu>
2021-12-12 15:28:10 -0800

    Web scraper cdm tests written. Not fully done

--- chargemaster/web_scraper/constant.py
+++ chargemaster/web_scraper/constant.py
@@ -1,8 +1,8 @@
 # Constant definitions
 MAX_RUN_TIME=480 # 480seconds = 8 minutes max runtime per hospital
-URLS_PATH = "data/hospital_urls.json"
-BLACKLIST_PATH = "web_scraper/blacklist.json"
-CHROMEDRIVER_PATH = "web_scraper/chromedriver"
+URLS_PATH = "./data/hospital_urls.json"
+BLACKLIST_PATH = "./chargemaster/web_scraper/blacklist.json"
+CHROMEDRIVER_PATH = "./chargemaster/web_scraper/chromedriver"
 
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
 HEADERS = {'User-Agent': USER_AGENT}

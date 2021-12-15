2ac369ef90364e4659a1d4caeb1196d61154fabb
Ji Kang <jkang18@uw.edu>
2021-12-14 18:58:26 -0800

    Modified constant.py to include git files

--- chargemaster/web_scraper/constant.py
+++ chargemaster/web_scraper/constant.py
@@ -6,9 +6,14 @@ CHROMEDRIVER_PATH = "./chargemaster/web_scraper/chromedriver"
 
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
 HEADERS = {'User-Agent': USER_AGENT}
-IGNORE_LIST = ['.config', 'drive', '.ipynb_checkpoints'] # Google Colab or Jupyter Notebook related files. Unrelated to scraped data
 
-SUBDIR_PATH = 'data/scraped_data'
+# Google Colab or Jupyter Notebook related files. Unrelated to scraped data
+IGNORE_LIST = ['.config', 'drive', '.ipynb_checkpoints', 
+                '.github', 'data', 'docs', 'examples', 
+                'src', '.gitignore', 'chargemaster', 'app.py',
+                'LICENSE', 'Procfile', 'README.md', 'requirements.txt', '.git']
+
+SUBDIR_PATH = './data/scraped_files'
 
 
 APX_KEYWORD = 'apps.para'

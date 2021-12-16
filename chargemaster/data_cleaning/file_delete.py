9e2c003f6383f9755c81c9ecc1959769966b6b40
Max Sgro <msgro@uw.edu>
2021-12-15 17:16:41 -0800

    updated file_delete.py with correct path reference

--- chargemaster/data_cleaning/file_delete.py
+++ chargemaster/data_cleaning/file_delete.py
@@ -12,32 +12,30 @@ import os
 def delete_files():
     """ This script iterates through all hospital folders in the directory path_name
     and deletes files that are not .csv or .xlsx"""
-    # path = pathlib.Path(path_name)
-    path = os.path.abspath(__file__+"/../../../data/scraped_data")
+    path = pathlib.Path(__file__).parents[2]/"data"/"scraped_data"
     isdir = os.path.isdir(path)
     dirname1 = os.path.basename(path)
     if isdir != True:
         raise ValueError ("Not a valid directory")
     if dirname1 != "scraped_data":
         raise ValueError ("Wrong Directory Specified")
+    #print(path)
     
     for folder in path.iterdir():
         if folder.is_dir():
-            #counter = 1
-            # folder_name = file.stem
-            # print("This is the folder name: ", folder_name)
+            
             for file in folder.iterdir():
                 if file.is_file():
                     if file.suffix == ".csv":
                        pass
-                       #print("this we keep: ", file.suffix)
+
                     elif file.suffix == ".xlsx":
                         pass
-                        #print("this we keep: ", file.suffix)
+
                     else:
-                        #print("this we delete: ", file.suffix)
+
                         file.unlink()
             
                     
 
-delete_files()
+#delete_files()
